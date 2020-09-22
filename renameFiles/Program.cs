cf8f5a089edd3c3ce11f36d0d2558330bd7e8b8d
Guilherme Schiavone <guilherme-schiavone@hotmail.com>
2020-09-21 22:35:56 -0300

    Implementação

---
+++ renameFiles/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace renameFiles
+{
+    class Program
+    {
+        private const string PATH = @"C:\Games\Stellaris\localisation\braz_por\";
+        private const string EXT = ".yml";
+
+        static void Main()
+        {
+            var files = Directory.GetFiles(PATH).ToList();
+
+            files.Where(x => x.Contains("l_braz_por")).ToList().ForEach(file =>
+            {
+                var nameRefatored = Path.GetFileNameWithoutExtension(file).Replace("braz_por", "english");
+                Directory.Move(file, PATH + nameRefatored + EXT);
+            });
+        }
+    }
+}
